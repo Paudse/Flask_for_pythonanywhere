d4c4a6e656f54bd64b2b881fb9a18d87baadb9ba
Paudse <b92209003@ntu.edu.tw>
2024-08-12 16:24:23 -0700

    ''

--- Learn_German.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-python ./Learn_German.py
-pause
--- flask_app_test.py
+++ flask_app_test.py
@@ -1,24 +1,24 @@
-# Flask網站前後端互動 09 - 超連結與圖片
-# 載入Flask、Request、render_template
 from flask import Flask, request, render_template
 
-# 建立 Application 物件,設定靜態檔案的路徑處理
-# http://127.0.0.1:5000/head.png 為圖片路徑
-# app = Flask(__name__, static_folder="public", static_url_path="/")
-app = Flask(__name__, template_folder='template')
-# 處理路徑 / 的對應函市
+app = Flask(__name__, template_folder='./html')
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def page_main():
+    return render_template("page_main.html")
 
+@app.route("/page_Learn_German")
+def page_Learn_German():
+    return render_template("page_Learn_German.html")
 
-@app.route("/page")
-def page():
-    name = request.args.get("nameis")
-    return render_template("page.html", namepage=name)
+# @app.route("/page")
+# def page():
+#     name = request.args.get("nameis")
+#     return render_template("page.html", namepage=name)
 
+# @app.route("/page_run_python_scripts")
+# def page_run_python_scripts():
+#     return render_template("page_run_python_scripts.html")
 
 # 啟動Server
 app.run()
